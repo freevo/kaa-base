e46ceeab0c68de45ff9db4605abcf0daea3abec0
Jason Tackaberry <tack@urandom.ca>
2005-07-22 15:53:28 +0000

    Make getch suck a little bit less.  Still needs work.  Will return strings now (translated keycode or the literal letter) instead of integers.
    git-svn-id: svn://svn.freevo.org/kaa/trunk/base@563 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/base/utils.py
+++ src/base/utils.py
@@ -33,29 +33,58 @@ import sys, tty, termios, select, fcntl, os, atexit, weakref, types
 _tc_orig_settings = None
 _getch_enabled = False
 
-def getch(timeout = None):
+_keycode_names = {
+    "\x1b\x5b\x41": "up",
+    "\x1b\x5b\x42": "down",
+    "\x1b\x5b\x43": "right",
+    "\x1b\x5b\x44": "left",
+
+    "\x1b\x4f\x50": "F1",
+    "\x1b\x4f\x51": "F2",
+    "\x1b\x4f\x52": "F3",
+    "\x1b\x4f\x53": "F4",
+    "\x1b\x5b\x31\x35\x7e": "F5",
+    "\x1b\x5b\x31\x37\x7e": "F6",
+    "\x1b\x5b\x31\x38\x7e": "F7",
+    "\x1b\x5b\x31\x39\x7e": "F8",
+    "\x1b\x5b\x32\x30\x7e": "F9",
+    "\x1b\x5b\x32\x31\x7e": "F10",
+    "\x1b\x5b\x32\x33\x7e": "F11",
+    "\x1b\x5b\x32\x34\x7e": "F12",
+
+    "\x1b\x5b\x32\x7e": "ins",
+    "\x1b\x5b\x33\x7e": "del",
+    "\x1b\x4f\x46": "end",
+    "\x1b\x4f\x48": "home",
+    "\x1b\x1b": "esc",
+    "\x0a": "enter",
+}
+    
+def getch():
     global _getch_enabled
+
     if not _getch_enabled:
         getch_enable()
         _getch_enabled = True
 
-    fd = sys.stdin.fileno()
+    buf = sys.stdin.read(1)
+    while buf in ("\x1b", "\x1b\x4f", "\x1b\x5b") or \
+          buf[:3] in ("\x1b\x5b\x31", "\x1b\x5b\x32", "\x1b\x5b\x33"):
+        buf += sys.stdin.read(1)
+        if buf[-1] == "\x7e":
+            break
 
-    if timeout != None:
-        (rfds, wfds, efds) = select.select([fd], [], [], timeout)
-        if rfds == []:
-            return False
-
-    ch = sys.stdin.read(1)
-    if ord(ch) == 27:
-        ch = sys.stdin.read(1)
-        if ch == '[':
-            ch = sys.stdin.read(1)
-            ch = ord(ch) + 255
-
-    if type(ch) == type(''):
-        return ord(ch)
-    return ch
+    #print "KEYCODE:"
+    #for c in buf:
+    #    print "  " +  hex(ord(c)) 
+    code = buf
+    #buf = ""
+    if code in _keycode_names:
+        return _keycode_names[code]
+    elif len(code) == 1:
+        return code
+    else:
+        return "??"
 
 
 def getch_enable():
@@ -65,14 +94,14 @@ def getch_enable():
     tc[3] = tc[3] & ~(termios.ICANON | termios.ECHO)
     tc[6][termios.VMIN] = 1
     tc[6][termios.VTIME] = 0
-    termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, tc)
+    termios.tcsetattr(sys.stdin.fileno(), termios.TCSANOW, tc)
     atexit.register(getch_disable)
     
 
 def getch_disable():
     global _tc_orig_settings
     if _tc_orig_settings != None:
-        termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, _tc_orig_settings)
+        termios.tcsetattr(sys.stdin.fileno(), termios.TCSANOW, _tc_orig_settings)
     os.system("stty echo")
 
     

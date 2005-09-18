af34dab099e572329a235c52786b6b0538085619
Dirk Meyer <dischi@freevo.org>
2005-09-18 10:53:03 +0000

    make sure we have a secure temp directory to play with
    git-svn-id: svn://svn.freevo.org/kaa/trunk/base@805 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,18 @@
+import os
+import stat
+
 from kaa.notifier import signals, loop as main, shutdown
+
+TEMP = '/tmp/kaa-%s' % os.getuid()
+
+if os.path.isdir(TEMP):
+    # temp dir is already there, check permissions
+    if os.path.islink(TEMP):
+        raise IOError('Security Error: %s is a link, please remove')
+    if stat.S_IMODE(os.stat(TEMP)[stat.ST_MODE]) != 0700:
+        raise IOError('Security Error: %s has wrong permissions, please remove')
+    if os.stat(TEMP)[stat.ST_UID] != os.getuid():
+        raise IOError('Security Error: %s does not belong to you, please remove')
+else:
+    os.mkdir(TEMP, 0700)
+

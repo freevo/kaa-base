e516292cb81254cc6dfec08d266124c8949ecdb1
Dirk Meyer <dischi@freevo.org>
2006-07-15 10:24:15 +0000

    adjust to moved fuctions/classes
    git-svn-id: svn://svn.freevo.org/kaa/trunk/base@1700 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/notifier/__init__.py
+++ src/notifier/__init__.py
@@ -50,10 +50,11 @@ from timer import *
 from sockets import *
 from event import *
 from yieldfunc import *
-from jobserver import *
+from jobserver import ThreadCallback, execute_in_thread
 from jobserver import killall as kill_jobserver
+from async import InProgress
 
-from decorators import execute_in_timer, execute_in_thread, execute_in_mainloop
+from decorators import execute_in_timer, execute_in_mainloop
 
 # get logging object
 log = logging.getLogger('notifier')

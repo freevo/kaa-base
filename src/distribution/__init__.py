907fa0a42dd90ca67d86e61ce7984d5764455fb9
Dirk Meyer <dischi@freevo.org>
2006-11-29 17:21:45 +0000

    add missing __init__.py
    git-svn-id: svn://svn.freevo.org/kaa/trunk/base@2117 a8f5125c-1e01-0410-8897-facf34644b8e

---
+++ src/distribution/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# core.py - distutils functions for kaa packages
+# -----------------------------------------------------------------------------
+# $Id: distribution.py 2110 2006-11-29 00:41:31Z tack $
+#
+# -----------------------------------------------------------------------------
+# Copyright (C) 2006 Dirk Meyer, Jason Tackaberry
+#
+# First Edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# Please see the file AUTHORS for a complete list of authors.
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version
+# 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+#
+# -----------------------------------------------------------------------------
+
+from core import *

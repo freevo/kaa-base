b1e9c269e7fa99e02a44db835d3f01ca52b509f2
Dirk Meyer <dischi@freevo.org>
2008-03-05 19:45:27 +0000

    more test cases
    git-svn-id: svn://svn.freevo.org/kaa/trunk/base@3162 a8f5125c-1e01-0410-8897-facf34644b8e

--- test/events.py
+++ test/events.py
@@ -1,15 +1,32 @@
 import kaa
 
-def foo(event):
+FOO_EVENT = kaa.Event('FOO_EVENT')
+
+def foobar(event):
     if event == 'FOO_EVENT':
         print 'got FOO'
     elif event == 'BAR_EVENT':
         print 'got BAR with %s' % event.arg
 
-e = kaa.EventHandler(foo)
+def foo(event):
+    if event == 'FOO_EVENT':
+        print 'foo got FOO'
+    else:
+        print 'This can not happen'
+
+def all(event):
+    print event
+    
+e = kaa.EventHandler(foobar)
 e.register(('FOO_EVENT', 'BAR_EVENT'))
 
-kaa.Event('FOO_EVENT').post()
+e = kaa.EventHandler(foo)
+e.register((FOO_EVENT,))
+
+e = kaa.EventHandler(all)
+e.register([])
+
+FOO_EVENT.post()
 kaa.Event('BAR_EVENT').post(1)
 kaa.Event('BAR_EVENT', 2).post()
 

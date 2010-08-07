d244c4338a44a9dead95504c969bd032bc856b65
Jason Tackaberry <tack@urandom.ca>
2010-08-07 00:21:54 +0000

    Several INotify changes:     - Allow multiple instances of INotify     - inherit from kaa.Object and implement signals with __kaasignals__     - add new INotify.mask_to_string() method     - improve documentation     - make number of arguments passed to signal callbacks consistent; it is       now always 3 (mask, filename, target) where target is usually None.
    git-svn-id: svn://svn.freevo.org/kaa/trunk/base@4311 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/extensions/inotify/__init__.py
+++ src/extensions/inotify/__init__.py
@@ -33,6 +33,7 @@ import fcntl
 import select
 import errno
 import socket
+import string
 
 # kaa imports
 import kaa
@@ -46,32 +47,57 @@ except ImportError:
 # get logging object
 log = logging.getLogger('inotify')
 
-# TODO: hook in gamin if it is running. See gamin.py
+class INotify(kaa.Object):
+    """
+    Monitor files and directories, invoking callbacks when changes occur.
 
-class INotify(object):
-    __instance = None  # Singleton
+    Multiple instances of this class can be created, but note that there is
+    a per-user limit of the number of INotify instances allowed, which is
+    controlled by /proc/sys/fs/inotify/max_user_instances
+    """
+    __kaasignals__ = {
+        'event':
+            '''
+            Emitted when an event occurs on any file or directory currently
+            being monitored by this INotify instance.
 
-    def __new__(cls):
-        if not INotify.__instance:
-            INotify.__instance = object.__new__(cls)
+            .. describe:: def callback(mask, filename, target)
 
-        return INotify.__instance
+               :param mask: a bitmask of events
+               :type mask: int
+               :param source: the filename the event applies to
+               :type filename: str
+               :param target: if the mask contains both MOVED_TO | MOVED_FROM,
+                              filename was renamed to target.  Otherwise,
+                              target is None.
+            '''
+    }
+
+    @staticmethod
+    def mask_to_string(mask):
+        """
+        Converts a bitmask of events to a human-readable string.
+
+        :param mask: the bitmask of events
+        :type mask: int
+        :returns: a string in the form EVENT1 | EVENT2 | EVENT3 ...
+        """
+        events = []
+        for attr in ['CHANGE'] + INotify.__dict__.keys():
+            if attr == 'WATCH_MASK' or attr[0] not in string.uppercase:
+                continue
+            event = getattr(INotify, attr)
+            if mask & event == event:
+                events.append(attr)
+                mask &= ~event
+        return ' | '.join(events)
 
 
     def __init__(self):
-        if hasattr(self, '_fd'):
-            # Constructor already called.  (We may be called again implicitly
-            # due to singleton behaviour in __new__
-            return
-
+        super(INotify, self).__init__()
         if not _inotify:
             self._fd = -1
-            raise SystemError, "INotify support not compiled."
-
-        self.signals = {
-            # Master signal: this signal gets emitted on all events.
-            "event": kaa.Signal()
-        }
+            raise SystemError('INotify support not compiled.')
 
         self._watches = {}
         self._watches_by_path = {}
@@ -87,7 +113,7 @@ class INotify(object):
         self._fd = _inotify.init()
 
         if self._fd < 0:
-            raise SystemError, "INotify support not detected on this system."
+            raise SystemError('INotify support not detected on this system.')
 
         fcntl.fcntl(self._fd, fcntl.F_SETFL, os.O_NONBLOCK)
         self._mon = kaa.WeakIOMonitor(self._handle_data)
@@ -101,14 +127,27 @@ class INotify(object):
             self._mon = None
 
 
-    def watch(self, path, mask = None):
+    def watch(self, path, mask=None):
         """
-        Adds a watch to the given path.  The default mask is anything that
-        causes a change (new file, deleted file, modified file, or attribute
-        change on the file).  This function returns a Signal that the caller
-        can then connect to.  Any time there is a notification, the signal
-        will get emitted.  Callbacks connected to the signal must accept 2
-        arguments: notify mask and filename.
+        Begin monitoring a file or directory for specific events.
+
+        :param path: the full path to the file or directory to be monitored
+        :type path: str
+        :param mask: a bitmask of events for which to notify, or None
+                     to use the default mask (see below).
+        :type mask: int
+        :returns: :class:`~kaa.Signal` object that is emitted when an event occurs
+                  on ``path``.
+
+        
+        The default mask is anything that causes a change (new file, deleted
+        file, modified file, or attribute change on the file).
+        
+        Callbacks connected to the returned signal are invoked with the same
+        arguments as the :attr:`~kaa.INotify.signals.event` signal.
+
+        The total number of watches (across all INotify instances) is controlled
+        by /proc/sys/fs/inotify/max_user_watches
         """
         path = os.path.realpath(path)
         if path in self._watches_by_path:
@@ -119,7 +158,7 @@ class INotify(object):
 
         wd = _inotify.add_watch(self._fd, path, mask)
         if wd < 0:
-            raise IOError, "Failed to add watch on '%s'" % path
+            raise IOError('Failed to add watch on "%s"' % path)
 
         signal = kaa.Signal()
         self._watches[wd] = [signal, path]
@@ -130,6 +169,11 @@ class INotify(object):
     def ignore(self, path):
         """
         Removes a watch on the given path.
+
+        :param path: the path that had been previously passed to 
+                     :meth:`~kaa.INotify.watch`
+        :type path: str
+        :returns: True if a matching monitor was removed, or False otherwise.
         """
         path = os.path.realpath(path)
         if path not in self._watches_by_path:
@@ -145,8 +189,12 @@ class INotify(object):
 
     def has_watch(self, path):
         """
-        Return if the given path is currently watched by the inotify
-        object.
+        Determine if the given path is currently watched by the INotify object.
+
+        :param path: the path that had been previously passed to 
+                     :meth:`~kaa.INotify.watch`
+        :type path: str
+        :returns: True if there is a matching monitor, or False otherwise.
         """
         path = os.path.realpath(path)
         return path in self._watches_by_path
@@ -155,6 +203,8 @@ class INotify(object):
     def get_watches(self):
         """
         Returns a list of all paths monitored by the object.
+
+        :returns: list of strings
         """
         return self._watches_by_path.keys()
 
@@ -168,7 +218,7 @@ class INotify(object):
 
         prev_wd, prev_mask, dummy, prev_path = self._move_state
         self._watches[prev_wd][0].emit(prev_mask, prev_path)
-        self.signals["event"].emit(prev_mask, prev_path)
+        self.signals["event"].emit(prev_mask, prev_path, None)
         self._move_state = None
         self._moved_timer.stop()
 
@@ -241,8 +291,8 @@ class INotify(object):
                 self._move_state = wd, mask, cookie, path
                 continue
 
-            self._watches[wd][0].emit(mask, path)
-            self.signals["event"].emit(mask, path)
+            self._watches[wd][0].emit(mask, path, None)
+            self.signals["event"].emit(mask, path, None)
 
             if mask & INotify.IGNORED:
                 # Self got deleted, so remove the watch data.

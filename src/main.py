freevo/kaa-base

commit cd9213ed71d3ba94aceb34426da6659e61d8e1fb

Put stop() in a OneShotTimer to mitigate possible deadlocks
Originally, stop() was decorated with @threaded(MAINTHREAD).  We take this one
step further by decorating it with @timed(policy=POLICY_ONCE) so that even if
stop() is invoked from the main thread, we wait until the current iteration is
finished before calling all the shutdown handlers.

This fixes at least one corner case deadlock, for example if stop() is called
from an asynchronous function like a signal handler.

src/main.py
@@ -303,7 +303,10 @@ def run(threaded=False):
             stop()
 
 
-@thread.threaded(thread.MAINTHREAD)
+# Put stop() in a timer to ensure we unravel the stack before shutting down.
+# This function does a lot of cleanup and there is a possibility of deadlocks
+# otherwise.
+@timer.timed(0, policy=timer.POLICY_ONCE)
 def stop():
     """
     Stop the main loop and terminate all child processes and thread

commit 1c994e95b07dca8ab7e683c88c243900031b5d8c

Introduce a thread event for shutdown
Fixes a regression in cd9213e because now stop() doesn't return an InProgress.
Also better document the shutdown process.

src/main.py
@@ -61,6 +61,7 @@ log = logging.getLogger('base')
 _running = None
 # Set if currently in shutdown() (to prevent reentrancy)
 _shutting_down = False
+_shutdown_event = threading.Event()
 # Lock preventing multiple threads from executing loop().
 _loop_lock = threading.Lock()
 # True if init() has been called
@@ -278,6 +279,7 @@ def run(threaded=False):
 
     global _shutting_down
     _shutting_down = False
+    _shutdown_event.clear()
 
     try:
         loop(True)
@@ -294,18 +296,21 @@ def run(threaded=False):
         except:
             pass
     finally:
-        # stop might be None if mainloop was run in a thread at interactive prompt
-        # (and therefore is a daemon thread).  In that case, we get here when
-        # the interpreter is shutting down, and we've shutdown enough that stop
-        # no longer exists.  It doesn't matter, because the atexit handler will
-        # have called stop already.
-        if stop:
-            stop()
+        # _stop might be None if mainloop was run in a daemon thread.  In that
+        # case, we get here when the interpreter is shutting down, and we've
+        # shutdown enough that stop no longer exists.  It doesn't matter,
+        # because the atexit handler will have called stop already.
+        if _stop:
+            _stop()
 
 
-# Put stop() in a timer to ensure we unravel the stack before shutting down.
-# This function does a lot of cleanup and there is a possibility of deadlocks
-# otherwise.
+
+# Put the public stop() in a timer to ensure we unravel the stack before
+# shutting down the notifier.
+#
+# notifier.shutdown() raises SystemExit which could get caught and handled in
+# undesirable ways by something else and the notifier loop may never even see
+# it.
 @timer.timed(0, policy=timer.POLICY_ONCE)
 def stop():
     """
@@ -314,6 +319,28 @@ def stop():
 
     Any notifier callback can also cause the main loop to terminate
     by raising SystemExit.
+    """
+    _stop()
+
+
+def _stop():
+    """
+    Internal function to shutdown the main loop.  This is called by stop()
+    (via a timer) and by run() when the notifier loop actually stops.  The
+    flow looks like:
+        1. user calls stop()
+        2. a one-shot timer is scheduled for _stop()
+        3. bext iteration of main loop, _stop() is called.
+        4. _shutting_down == False and is_running() == True, so
+           notifier.shutdown() is invoked.
+        5. notifier.shutdown() raises SystemExit which aborts _stop() and
+           is caught by the notifier loop.
+        6. notifier loop() returns
+        7. 'finally' block in run() invokes _stop()
+        8. _shutting_down == False and is_running() == False, so we set
+           _shutting_down = True and proceed with cleanup.
+        9. set the _shutdown_event so anyway threads waiting in
+           _shutdown_check() can resume.
     """
     global _shutting_down
 
@@ -342,6 +369,7 @@ def stop():
         os.waitpid(-1, os.WNOHANG)
     except OSError:
         pass
+    _shutdown_event.set()
 
 
 def step(*args, **kwargs):
@@ -425,12 +453,11 @@ def _shutdown_check(*args):
         # SystemExit and things will exit normally.
         if CoreThreading.is_mainthread():
             _set_running(False)
-        r = stop()
-        if r:
-            # If stop() returns non-None, it's an InProgress, which means the
-            # main loop was running in a separate thread.  Wait for it to
-            # finish.
-            r.wait()
+        _stop()
+        if not CoreThreading.is_mainthread():
+            # Main loop is running in another thread, so we need to wait for it
+            # to finish.
+            _shutdown_event.wait()
 
 
 # check to make sure we really call our shutdown function
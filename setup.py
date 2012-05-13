8399342e4cc932c22935f45cd48ddaa770b700a1
Jason Tackaberry <tack@urandom.ca>
2012-05-13 17:53:59 -0400

    Automatically construct dev version number if installing from git

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@
 # -----------------------------------------------------------------------------
 # setup.py - Setup script for kaa.base
 # -----------------------------------------------------------------------------
-# $Id$
-#
-# -----------------------------------------------------------------------------
 # Copyright 2005-2012 Dirk Meyer, Jason Tackaberry
 #
 # This library is free software; you can redistribute it and/or modify
@@ -22,6 +19,9 @@
 # 02110-1301 USA
 #
 # -----------------------------------------------------------------------------
+
+MODULE = 'base'
+VERSION = '0.99.2'
 
 # python imports
 import sys
@@ -103,9 +103,27 @@ if sys.hexversion < 0x02060000:
     if utils_ext.has_python_h():
         extensions.append(utils_ext)
 
+# Automagically construct version.  If installing from a git clone, the
+# version is based on the number of revisions since the last tag.  Otherwise,
+# if PKG-INFO exists, assume it's a dist tarball and pull the version from
+# that.
+version = VERSION
+if os.path.isdir('.git'):
+    # Fetch all revisions since last tag.  The first item is the current HEAD object name
+    # and the last is the tag object name.
+    revs = os.popen('git rev-list --all | grep -B9999  `git rev-list --tags --max-count=1`').read().splitlines()
+    if len(revs) > 1:
+        # We're at least one commit past the last tag, so this is considered a
+        # dev release.
+        version = '%sdev-%d-%s' % (VERSION, len(revs)-1, revs[0][:8])
+elif os.path.isfile('PKG-INFO'):
+    ver = [l.split(':')[1].strip() for l in open('PKG-INFO') if l.startswith('Version')]
+    if ver:
+        version = ver[0]
+
 setup(
-    module = 'base',
-    version = '0.99.2',
+    module = MODULE,
+    version = version,
     license = 'LGPL',
     url = 'http://doc.freevo.org/api/kaa/base/',
     summary = 'An application framework specializing in asynchronous programming.',

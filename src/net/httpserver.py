2a4ec039e2bea2aeb471321a3761308682f8a6a5
Dirk Meyer <dischi@freevo.org>
2012-09-15 17:53:19 +0200

    code cleanup

--- src/net/httpserver.py
+++ src/net/httpserver.py
@@ -29,10 +29,12 @@
 # python imports
 import os
 import logging
+import socket
 import BaseHTTPServer
 import SocketServer
 import shutil
 import urlparse
+import json
 
 # kaa imports
 import kaa
@@ -52,57 +54,69 @@ class ThreadedHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         """
         callback = None
         parse_result = urlparse.urlparse(self.path)
-        for path, callback in self.server._get_handler:
+        for path, callback, ctype, encoding in self.server._get_handler:
             if path == parse_result.path or \
                     (parse_result.path.startswith(path) and path.endswith('/')):
                 path = parse_result.path[len(path):]
-                break
-        else:
-            callback = None
-        if not callback:
-            if self.path in self.server._static:
+                try:
+                    attributes = {}
+                    for key, value in urlparse.parse_qs(parse_result.query).items():
+                        if isinstance(value, (list, tuple)):
+                            if len(value) == 0:
+                                attributes[key] = True
+                            elif len(value) == 1:
+                                attributes[key] = value[0]
+                            else:
+                                attributes[key] = value
+                        else:
+                            attributes[key] = value
+                    result = kaa.MainThreadCallable(callback)(path, **attributes).wait()
+                except:
+                    self.send_response(500)
+                    log.exception('server error')
+                    return
+                # we need content, content type and content encoding
+                # as result if not already provided when registering
+                # the callback
+                if ctype is None or encoding is None:
+                    # return must be a list with the actual content as
+                    # first item and ctype and encoding if missing
+                    # following.
+                    if encoding is None:
+                        encoding = result.pop()
+                    if ctype is None:
+                        ctype = result.pop()
+                    result = result[0]
+                if ctype == "application/json":
+                    result = json.dumps(result)
                 self.send_response(200)
+                self.send_header("Content-type", ctype)
+                self.send_header("Content-Length", len(result))
+                self.send_header("Content-Encoding", encoding)
                 self.end_headers()
-                f = open(self.server._static[self.path])
-                shutil.copyfileobj(f, self.wfile)
-                f.close()
+                self.wfile.write(result)
                 return
-            abspath = os.path.abspath(self.path)
-            for path, dirname in self.server._directories:
-                if abspath.startswith(path):
-                    fname = os.path.join(dirname, abspath[len(path)+1:])
-                    if os.path.isfile(fname):
-                        self.send_response(200)
-                        self.end_headers()
-                        f = open(fname)
-                        shutil.copyfileobj(f, self.wfile)
-                        f.close()
-                        return
-            self.send_response(404)
+        if self.path in self.server._static:
+            self.send_response(200)
             self.end_headers()
+            f = open(self.server._static[self.path])
+            shutil.copyfileobj(f, self.wfile)
+            f.close()
             return
-        try:
-            attributes = {}
-            for key, value in urlparse.parse_qs(parse_result.query).items():
-                if isinstance(value, (list, tuple)):
-                    if len(value) == 0:
-                        attributes[key] = True
-                    elif len(value) == 1:
-                        attributes[key] = value[0]
-                    else:
-                        attributes[key] = value
-                else:
-                    attributes[key] = value
-            ctype, content = kaa.MainThreadCallable(callback)(path, **attributes).wait()
-        except:
-            self.send_response(500)
-            log.exception('server error')
-            return
-        self.send_response(200)
-        self.send_header("Content-type", ctype)
-        self.send_header("Content-Length", len(content))
+        abspath = os.path.abspath(self.path)
+        for path, dirname in self.server._directories:
+            if abspath.startswith(path):
+                fname = os.path.join(dirname, abspath[len(path)+1:])
+                if os.path.isfile(fname):
+                    self.send_response(200)
+                    self.end_headers()
+                    f = open(fname)
+                    shutil.copyfileobj(f, self.wfile)
+                    f.close()
+                    return
+        self.send_response(404)
         self.end_headers()
-        self.wfile.write(content)
+        return
 
     def log_message(self, format, *args):
         """
@@ -114,42 +128,35 @@ class ThreadedHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 class HTTPServer(SocketServer.ThreadingTCPServer):
     """
     HTTPServer
-
-    Example::
-
-      server = kaa.net.httpserver.HTTPServer(("", port), MyRequestHandler)
-      server.serve_forever()  # hooks into kaa mainloop
-
-      def callback1(path):
-          '''
-          callback1 handles GET /foo
-          '''
-          return 'text/plain', 'foo was called'
-
-      @kaa.coroutine()
-      def callback2(path):
-          '''
-          callback2 handles GET /bar/ as well as /bar/something
-          '''
-          yield waitforsomething
-          yield 'text/plain', 'bar was called with ' + path
-
-      # add callback1 and callback2 to the server
-      server.add_handler('foo', callback1)
-      server.add_handler('bar/', callback2)
-
     """
     allow_reuse_address = True
     daemon_threads = True
 
-    def __init__(self, server_address, RequestHandlerClass=ThreadedHTTPRequestHandler, bind_and_activate=True):
+    def __init__(self, server_address, RequestHandlerClass=ThreadedHTTPRequestHandler,
+                 bind_and_activate=True):
         """
         Create the HTTPServer
         """
         self._get_handler = []
         self._static = {}
         self._directories = []
-        SocketServer.ThreadingTCPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate)
+        SocketServer.ThreadingTCPServer.__init__(
+            self, server_address, RequestHandlerClass, bind_and_activate)
+
+    def process_request_thread(self, request, client_address):
+        """
+        Rewrite of process_request_thread from the ThreadingTCPServer
+        handling socket.error by ignoring it.
+        """
+        try:
+            self.finish_request(request, client_address)
+            self.shutdown_request(request)
+        except socket.error:
+            # the other side did not want to wait long enough
+            pass
+        except:
+            self.handle_error(request, client_address)
+            self.shutdown_request(request)
 
     def __handle_request(self, f):
         """
@@ -164,11 +171,17 @@ class HTTPServer(SocketServer.ThreadingTCPServer):
         """
         notifier.socket_add(self.fileno(), self.__handle_request)
 
-    def add_handler(self, path, callback):
+    def add_handler(self, path, callback, ctype=None, encoding=None):
         """
         Add a handler for path
         """
-        self._get_handler.append((path, callback))
+        self._get_handler.append((path, callback, ctype, encoding))
+
+    def add_json_handler(self, path, callback):
+        """
+        Add a handler for path
+        """
+        self._get_handler.append((path, callback, 'application/json', 'utf-8'))
 
     def add_static(self, path, filename):
         """

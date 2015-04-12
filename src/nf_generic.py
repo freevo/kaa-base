349d23f7cfa12f27704c89e881bdac5dd138a4a6
Dirk Meyer <dischi@freevo.org>
2015-04-12 09:47:42 +0200

    Remove pynotifier subpackage
    The pynotifier code is outdated. Besides the generic notifier
    the others are not wirking anymore. Therefore, the pynotifier
    code is stripped down to be added directly in kaa.

---
+++ src/nf_generic.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Andreas Büsching <crunchy@bitkipper.net>
+#
+# generic notifier implementation
+#
+# Copyright 2004, 2005, 2006, 2007
+#    Andreas Büsching <crunchy@bitkipper.net>
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version
+# 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+
+"""Simple mainloop that watches sockets and timers."""
+from __future__ import absolute_import
+
+# python core packages
+import logging
+from select import select
+from select import error as select_error
+from time import time, sleep as time_sleep
+import errno, os, sys
+
+import socket
+
+# get logging object
+log = logging.getLogger('kaa.base.core.main')
+
+IO_READ = 1
+IO_WRITE = 2
+IO_EXCEPT = 4
+( INTERVAL, TIMESTAMP, CALLBACK ) = range( 3 )
+
+__sockets = {}
+__sockets[ IO_READ ] = {}
+__sockets[ IO_WRITE ] = {}
+__sockets[ IO_EXCEPT ] = {}
+__timers = {}
+__timer_id = 0
+__min_timer = None
+__in_step = False
+__step_depth = 0
+__step_depth_max = 5
+
+def socket_add( id, method, condition = IO_READ ):
+    """The first argument specifies a socket, the second argument has to be a
+    function that is called whenever there is data ready in the socket.
+    The callback function gets the socket back as only argument."""
+    global __sockets
+    __sockets[ condition ][ id ] = method
+
+def socket_remove( id, condition = IO_READ ):
+    """Removes the given socket from scheduler. If no condition is specified the
+    default is IO_READ."""
+    global __sockets
+    if id in __sockets[ condition ]:
+        del __sockets[ condition ][ id ]
+
+def timer_add( interval, method ):
+    """The first argument specifies an interval in milliseconds, the second
+    argument a function. This is function is called after interval
+    seconds. If it returns true it's called again after interval
+    seconds, otherwise it is removed from the scheduler. The third
+    (optional) argument is a parameter given to the called
+    function. This function returns an unique identifer which can be
+    used to remove this timer"""
+    global __timer_id
+
+    try:
+        __timer_id += 1
+    except OverflowError:
+        __timer_id = 0
+
+    __timers[ __timer_id ] = \
+    [ interval, int( time() * 1000 ) + interval, method ]
+
+    return __timer_id
+
+def timer_remove( id ):
+    """Removes the timer identifed by the unique ID from the main loop."""
+    if id in __timers:
+        del __timers[ id ]
+
+def step( sleep = True, external = True, simulate = False ):
+    """Do one step forward in the main loop. First all timers are checked for
+    expiration and if necessary the accociated callback function is called.
+    After that the timer list is searched for the next timer that will expire.
+    This will define the maximum timeout for the following select statement
+    evaluating the registered sockets. Returning from the select statement the
+    callback functions from the sockets reported by the select system call are
+    invoked. As a final task in a notifier step all registered external
+    dispatcher functions are invoked."""
+
+    global __in_step, __step_depth, __step_depth_max
+
+    __in_step = True
+    __step_depth += 1
+
+    try:
+        if __step_depth > __step_depth_max:
+            log.exception( 'maximum recursion depth reached' )
+            return
+
+        # get minInterval for max timeout
+        timeout = None
+        if not sleep:
+            timeout = 0
+        else:
+            now = int( time() * 1000 )
+            for interval, timestamp, callback in __timers.values():
+                if not timestamp:
+                    # timer is blocked (recursion), ignore it
+                    continue
+                nextCall = timestamp - now
+                if timeout == None or nextCall < timeout:
+                    if nextCall > 0:
+                        timeout = nextCall
+                    else:
+                        timeout = 0
+                        break
+            if timeout == None:
+                                # No timers, timeout could be infinity.
+                                timeout = 30000
+            if __min_timer and __min_timer < timeout: timeout = __min_timer
+
+
+        # wait for event
+        sockets_ready = None
+        if __sockets[ IO_READ ] or __sockets[ IO_WRITE ] or __sockets[ IO_EXCEPT ]:
+            try:
+                sockets_ready = select( __sockets[ IO_READ ].keys(), __sockets[ IO_WRITE ].keys(),
+                                        __sockets[ IO_EXCEPT ].keys(), timeout / 1000.0 )
+            except select_error, e:
+                if e.args[ 0 ] != errno.EINTR:
+                    raise e
+        elif timeout:
+            time_sleep(timeout / 1000.0)
+
+        if simulate:
+            # we only simulate
+            return
+        
+        # handle timers
+        for i, timer in __timers.items():
+            timestamp = timer[ TIMESTAMP ]
+            if not timestamp or i not in __timers:
+                # timer was unregistered by previous timer, or would
+                # recurse, ignore this timer
+                continue
+            now = int( time() * 1000 )
+            if timestamp <= now:
+                # Update timestamp on timer before calling the callback to
+                # prevent infinite recursion in case the callback calls
+                # step().
+                timer[ TIMESTAMP ] = 0
+                if not timer[ CALLBACK ]():
+                    if i in __timers:
+                        del __timers[ i ]
+                else:
+                    # Find a moment in the future. If interval is 0, we
+                    # just reuse the old timestamp, doesn't matter.
+                    if timer[ INTERVAL ]:
+                        now = int( time() * 1000 )
+                        timestamp += timer[ INTERVAL ]
+                        while timestamp <= now:
+                            timestamp += timer[ INTERVAL ]
+                    timer[ TIMESTAMP ] = timestamp
+
+        # handle sockets
+        if sockets_ready:
+            for condition, sockets in zip((IO_READ, IO_WRITE, IO_EXCEPT), sockets_ready):
+                for sock in sockets:
+                    # XXX: Not quite sure why these checks are done, since select()
+                    # would have raised on these first.
+                    try:
+                        if isinstance(sock, int):
+                            assert(sock != -1)
+                        else:
+                            assert(getattr(sock, 'closed', False) == False)
+                            assert(sock.fileno() != -1)
+                    except (AssertionError, socket.error, ValueError):
+                        # socket is either closed or not supported.
+                        socket_remove( sock, condition )
+                        continue
+
+                    # the timer handling might have removed a socket from the
+                    # list and therefore sock is not in __sockets[ condition ]
+                    # anymore.
+                    callback = __sockets[ condition ].get(sock)
+                    if callback is not None and not callback( sock ):
+                        socket_remove( sock, condition )
+        
+    finally:
+        __step_depth -= 1
+        __in_step = False
--- src/nf_wrapper.py
+++ src/nf_wrapper.py
@@ -32,6 +32,8 @@ import atexit
 # notifier import
 from .callable import Callable, WeakCallable, CallableError
 from .utils import property
+# use our own copy of pynotifier
+from . import nf_generic
 
 # get logging object
 log = logging.getLogger('kaa.base.core.main')
@@ -44,16 +46,15 @@ log = logging.getLogger('kaa.base.core.main')
 # is None.
 _python_shutting_down = False
 
-# The name of the module init() was called with, or None if init() has not
-# been called yet.
-loaded = None
+loaded = False
 
 class NotifierCallback(Callable):
-
+    """
+    Callback class for the generic notifier
+    """
     def __init__(self, callback, *args, **kwargs):
         super(NotifierCallback, self).__init__(callback, *args, **kwargs)
         self._id = None
-
 
     @property
     def active(self):
@@ -64,18 +65,15 @@ class NotifierCallback(Callable):
         # if python is in shutdown, notifier unregister could crash
         return self._id != None and _python_shutting_down == False
 
-
     def unregister(self):
         # Unregister callback with notifier.  Must be implemented by subclasses.
         self._id = None
-
 
     def __call__(self, *args, **kwargs):
         if not self._get_func():
             if self.active:
                 self.unregister()
             return False
-
         try:
             ret = super(NotifierCallback, self).__call__(*args, **kwargs)
         except CallableError:
@@ -90,28 +88,19 @@ class NotifierCallback(Callable):
 
 
 class WeakNotifierCallback(WeakCallable, NotifierCallback):
-
+    """
+    Weak Callback class for the generic notifier
+    """
     def _weakref_destroyed(self, object):
         if _python_shutting_down == False:
             super(WeakNotifierCallback, self)._weakref_destroyed(object)
             self.unregister()
 
 
-class _Wrapper(object):
-    def __init__(self, name):
-        self.name = name
+timer_remove = nf_generic.timer_remove
+timer_add = nf_generic.timer_add
 
-    def __call__(self, *args, **kwargs):
-        init()
-        return globals()[self.name](*args, **kwargs)
-
-dispatcher_add = _Wrapper('dispatcher_add')
-dispatcher_remove = _Wrapper('dispatcher_remove')
-step = _Wrapper('step')
-timer_remove = _Wrapper('timer_remove')
-timer_add = _Wrapper('timer_add')
-socket_remove = _Wrapper('socket_remove')
-socket_add = _Wrapper('socket_add')
+step = nf_generic.step
 
 def shutdown():
     # prefered way to shut down the system
@@ -127,82 +116,15 @@ def _socket_add(id, method, condition = 0):
 def _socket_remove(id, condition = 0):
     return nf_socket_remove(id, nf_conditions[condition])
 
+nf_socket_remove = nf_generic.socket_remove
+nf_socket_add = nf_generic.socket_add
+nf_conditions = [ nf_generic.IO_READ, nf_generic.IO_WRITE, nf_generic.IO_EXCEPT ]
+socket_remove = _socket_remove
+socket_add = _socket_add
+
 
 def init( module = None, force_internal=False, **options ):
-    global timer_add
-    global socket_add
-    global dispatcher_add
-    global timer_remove
-    global socket_remove
-    global dispatcher_remove
-    global step
-    global nf_socket_remove
-    global nf_socket_add
-    global nf_conditions
-    global shutdown
-    global loaded
-
-    if not isinstance(timer_add, _Wrapper):
-        raise RuntimeError('notifier already initialized')
-
-    if not 'recursive_depth' in options:
-        # default value of 2 is not enough when using async yield stuff
-        options['recursive_depth'] = 5
-
-    try:
-        if force_internal:
-            # pynotifier is not allowed
-            raise ImportError()
-        import notifier
-        if notifier.loop:
-            raise RuntimeError('pynotifier already initialized')
-    except ImportError:
-        # use our own copy of pynotifier
-        from . import pynotifier as notifier
-
-    # find a good main notifier implementation
-    if not module:
-        module = 'generic'
-        if 'gtk' in sys.modules:
-            # The gtk module is loaded, this means that we will hook
-            # ourself into the gtk notifier
-            module = 'gtk'
-            log.info('Implicitly using gtk integration for the notifier')
-
-    if not module in ('generic', 'gtk', 'twisted_experimental'):
-        raise AttributeError('unsupported notifier %s' % module)
-
-    if module == 'twisted_experimental':
-        module = 'twisted'
-
-    # use the selected module
-    notifier.init(getattr(notifier, module.upper()), **options)
-
-    # delete basic notifier handler
-    nlog = logging.getLogger('notifier')
-    for l in nlog.handlers:
-        nlog.removeHandler(l)
-
-    timer_remove = notifier.timer_remove
-    timer_add = notifier.timer_add
-
-    nf_socket_remove = notifier.socket_remove
-    nf_socket_add = notifier.socket_add
-    nf_conditions = [ notifier.IO_READ, notifier.IO_WRITE, notifier.IO_EXCEPT ]
-    socket_remove = _socket_remove
-    socket_add = _socket_add
-
-    dispatcher_add = notifier.dispatcher_add
-    dispatcher_remove = notifier.dispatcher_remove
-
-    step = notifier.step
-
-    if module == 'twisted':
-        # special stop handling for twisted
-        from twisted.internet import reactor
-        shutdown = reactor.stop
-
-    loaded = module
+    loaded = True
 
 
 def _shutdown_weakref_destroyed():
